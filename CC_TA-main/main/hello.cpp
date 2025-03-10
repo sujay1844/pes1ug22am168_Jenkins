955e0600ee2c7351b486cb433b3d4bcb5d9027be
Rosh <roshniraju700@gmail.com>
2025-03-10 15:38:30 +0530

    pes1ug22am135 has added the files

---
+++ CC_TA-main/main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout <<"this is roshni my srn is pes1ug22am135 \n";
+
+  return 0;
+}
