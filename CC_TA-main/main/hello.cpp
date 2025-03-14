825646355b5b032c74b6df1c37938bdb1829b984
Roshni R <136987759+Roshr2211@users.noreply.github.com>
2025-03-14 19:34:38 +0530

    Update hello.cpp

--- CC_TA-main/main/hello.cpp
+++ CC_TA-main/main/hello.cpp
@@ -6,8 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout <<"this is roshni my srn is pes1ug22am135 \n";
-  cout<<"i have made changes for task4\n";
 
   return 0;
 }
